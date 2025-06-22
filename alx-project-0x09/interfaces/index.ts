589789d247ad366907e665c9fca45d10ac9c8b15
Prince <uchepibe@gmail.com>
2025-06-22 05:42:48 +0100

    Generate Image based on user prompt

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -10,7 +10,10 @@ export interface GeneratedImageProps{
     width?: string;
     height?: string;
     action: (imagePath: string) => void;
+}
 
+export type RequestProps = {
+    prompt: string;
 }
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -9,15 +9,31 @@ const Home: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleGenerateImage = async () => {
-    console.log("Generating Images");
-    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+    setIsLoading(true);
+    const resp = await fetch('/api/generate-image',{
+      method: 'POST',
+      body:JSON.stringify({
+        prompt
+      }),
+      headers:{
+        'Content-type': 'application/json'
+      }
+    })
+
+    if(!resp.ok){
+      setIsLoading(false)
+      return;
+    }
+
+    const data = await resp.json()
+    setIsLoading(false)
   };
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
-        <p className="text-lg text-gray-700 mb-4">
+        <h1 className="text-4xl font-bold mb-2 text-black">Image Generation App</h1>
+        <p className="text-lg text-black mb-4">
           Generate stunning images based on your prompts!
         </p>
 
@@ -27,15 +43,15 @@ const Home: React.FC = () => {
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
-            className="w-full p-3 border border-black-300 rounded-lg mb-4"
+            className="w-full p-3 border border-black-300 rounded-lg mb-4 text-black"
           />
           <button
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
-            {/* {
+            {
               isLoading ? "Loading..." : "Generate Image"
-            } */}
+            }
             Generate Image
           </button>
         </div>
